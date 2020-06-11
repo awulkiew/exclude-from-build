86cc5d4113a6c796f730215e83a549185e0c179a
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2020-06-11 02:08:33 +0200

    Support VS2013 and VS2015.
    Implement setting of ExcludeFromBuild property differently.
    Change dependencies.
    Bump version.

--- Visual_Studio_2017/ExcludeFromBuild/Util.cs
+++ Visual_Studio_2017/ExcludeFromBuild/Util.cs
@@ -6,28 +6,45 @@
 
 using EnvDTE;
 using EnvDTE80;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.VCProjectEngine;
 using System;
 using System.Collections;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace ExcludeFromBuild
 {
     class Util
     {
+        class ConfigInfo
+        {
+            public string Configuration;
+            public string Platform;
+            public ConfigInfo(string configuration, string platform)
+            {
+                Configuration = configuration;
+                Platform = platform;
+            }
+        }
+
 #pragma warning disable VSTHRD010
-        public static void SetExcludedFromBuild(DTE2 dte, bool value)
+        public static void SetExcludedFromBuild(DTE2 dte, bool value, bool setAllCongurations = false)
         {
             if (dte == null)
                 return;
             var items = dte.ToolWindows.SolutionExplorer.SelectedItems as Array;
             if (items == null)
                 return;
-            SetExcludedFromBuildRecursive(items, value);
+            SetExcludedFromBuildRecursive(items, value, setAllCongurations);
         }
 
-        private static void SetExcludedFromBuildRecursive(IEnumerable items, bool value)
+        // Casting COM objects to VCFile and VCFilter works but the problem is that
+        // for VS2017 and VS2019 one has to add different Microsoft.VisualStudio.VCProjectEngine
+        // version than for VS2015. Otherwise casting resutls in null object.
+        // So below call properties of CPM objects manually.
+
+        private static void SetExcludedFromBuildRecursive(IEnumerable items,
+                                                          bool value,
+                                                          bool setAllCongurations)
         {
             foreach (var item in items)
             {
@@ -37,22 +54,52 @@ namespace ExcludeFromBuild
                     var pitem = hitem.Object as ProjectItem;
                     if (pitem != null)
                     {
-                        VCFilter filter = pitem.Object as VCFilter;
-                        if (filter != null)
+                        if (hitem.UIHierarchyItems.Count > 0)
                         {
-                            SetExcludedFromBuildRecursive(hitem.UIHierarchyItems, value);
+                            SetExcludedFromBuildRecursive(hitem.UIHierarchyItems, value, setAllCongurations);
                         }
                         else
                         {
-                            VCFile file = pitem.Object as VCFile;
-                            if (file != null)
+                            var kind = pitem.Object.GetType().InvokeMember("Kind",
+                                            BindingFlags.GetProperty,
+                                            null, pitem.Object, null) as string;
+                            if (kind == "VCFile")
                             {
-                                foreach (var f in file.FileConfigurations)
+                                var activeConfig = pitem.ContainingProject.ConfigurationManager.ActiveConfiguration;
+                                string activeConfigName = activeConfig.ConfigurationName;
+                                string activePlatformName = activeConfig.PlatformName;
+
+                                var fileConfigurations = pitem.Object.GetType().InvokeMember("FileConfigurations",
+                                                            BindingFlags.GetProperty,
+                                                            null, pitem.Object, null) as IEnumerable;
+
+                                foreach (var c in fileConfigurations)
                                 {
-                                    var fc = f as VCFileConfiguration;
-                                    if (fc != null)
+                                    bool set = setAllCongurations;
+                                    if (!setAllCongurations)
                                     {
-                                        fc.ExcludedFromBuild = value;
+                                        var config = c.GetType().InvokeMember("ProjectConfiguration",
+                                                        BindingFlags.GetProperty,
+                                                        null, c, null);
+                                        var cPath = config.GetType().InvokeMember("PersistPath",
+                                                        BindingFlags.GetProperty,
+                                                        null, config, null) as string;
+                                        var cName = config.GetType().InvokeMember("ConfigurationName",
+                                                        BindingFlags.GetProperty,
+                                                        null, config, null) as string;
+                                        var cPlat = config.GetType().InvokeMember("PlatformName",
+                                                        BindingFlags.GetProperty,
+                                                        null, config, null) as string;
+
+                                        set = activeConfigName == cName
+                                                && activePlatformName == cPlat;
+                                    }
+
+                                    if (set)
+                                    {
+                                        c.GetType().InvokeMember("ExcludedFromBuild",
+                                                BindingFlags.SetProperty,
+                                                null, c, new object[] { value });
                                     }
                                 }
                             }
